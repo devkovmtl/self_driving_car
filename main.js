7e3d13349ecc1c3766a603c94dca0e86a69ed648
Mikael <dev.kov.mtl@gmail.com>
2022-05-16 14:37:40 -0400

    Defining number road

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 30, 50); // pos x, y, 30 w, 50h
-car.draw(ctx);
+
+// road
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+// car
+// const car = new Car(100, 100, 30, 50); // pos x, y, 30 w, 50h
+const car = new Car(road.getLaneCenter(1), 100, 30, 50); // pos x, y, 30 w, 50h
 
 animate();
 
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
